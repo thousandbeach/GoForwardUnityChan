37d2254d9fe0cebbd7522e6efdadfdb4e7fd848c
Takahisa Fujiwara <thousandbeach.td@gmail.com>
2017-02-25 13:08:01 +0900

    Goforwardunityphan firstCommit

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	// スクロール速度
+	private float scrollSpeed= -0.03f;
+
+	// 背景終了位置
+	private float deadLine = -16f;
+
+	// 背景開始位置
+	private float startline = 15.8f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// 背景を移動する
+		transform.Translate(this.scrollSpeed, 0, 0);
+
+		// 画面外にでたら、画面右側に移動する
+		if(transform.position.x < this.deadLine){
+			transform.position = new Vector2 (this.startline, 0);
+		}
+
+	}
+}
---
+++ Assets/UnityChan2D/Demo/Sprites/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// オーディオソースのオブジェクトを取得（音の出し方の情報）
+	public AudioSource audioSource;
+
+	// オーディクリップを取得（音自体の情報 音のファイル）
+	public AudioClip grave;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// キューブを移動させる
+		transform.Translate(this.speed, 0, 0);
+
+		// 画面外にでたら破滅する
+		if(transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	// 2Dの場合はコリジョンの後に2Dは必須
+	void OnCollisionEnter2D(Collision2D other){
+		// グラウンドタグかキューブタグに衝突したときに音をならす unity側でタグを設定しておく
+		if(other.transform.tag == "GroundTag" || other.transform.tag == "CubeTag"){
+			audioSource.PlayOneShot (grave);
+		}
+	}
+
+}
